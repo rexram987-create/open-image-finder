fd19bea3083a96365d2093ca0af51f0c06a82324
rexram987-create <rexram987@gmail.com>
2026-09-11 10:28:03 +0300

    feat: localize the app shell

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { useI18n } from './i18n/useI18n';
+
 export default function App() {
+  const { t } = useI18n();
+
   return (
     <main className="app-shell">
-      <h1>חיפוש תמונות חופשיות</h1>
+      <h1>{t('appTitle')}</h1>
     </main>
   );
 }
