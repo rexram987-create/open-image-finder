23568f080b4aff4074dc5b0d2f7747c914e7ffd3
rexram987-create <rexram987@gmail.com>
2026-09-12 17:27:23 +0300

    Fix syntax error in Chicago image proxy URLs

--- app.js
+++ app.js
@@ -250,8 +250,8 @@ async function aicPages(term,limit=8){
       title:'File:'+(x.title||('Art Institute of Chicago artwork '+x.id)),
       _source:'aic',
       imageinfo:[{
-        url:'/api/aic-image?id='+x.image_id',
-        thumburl:'/api/aic-image?id='+x.image_id',
+        url:'/api/aic-image?id='+x.image_id,
+        thumburl:'/api/aic-image?id='+x.image_id,
         descriptionurl:'https://www.artic.edu/artworks/'+x.id,
         extmetadata:{
           LicenseShortName:{value:'Public domain'},
